923105a7a23a0619db9bca83ee3747210eca7cba
HTMLjsgit <53972763+HTMLjsgit@users.noreply.github.com>
2019-12-03 20:37:00 +0900

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,32 @@
+with open('dictionaly.txt') as open_file:
+  all_data = open_file.read()
+
+line_list = all_data.splitlines()
+
+bot_dict = {}
+
+for line in line_list:
+  orig,trans = line.split(':')
+  bot_dict[orig] = trans
+
+while True:
+  command = input('メッセージを入力: ')
+
+  responce = ""
+
+  for key in bot_dict:
+    if key in command:
+      responce = bot_dict[key]
+      print(responce)
+      break
+    # else:
+    #   print('なんていいましたか')
+
+if not responce:
+  responce = "あなたとはなしがしたい"
+
+print(responce)
+
+if 'さようなら' in command:
+  print("じゃあね！")
+  break
